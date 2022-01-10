c4cdd4b7d91947ea5e8e67f009fe2aa3c219ac46
iliya <iliya2012hajjar@gmail.com>
2022-01-10 17:31:06 +0330

    feat: update README anarticles

--- simple_articles/urls.py
+++ simple_articles/urls.py
@@ -3,14 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from articles.views import ArticleListView
 
-urlpatterns = [
+
+urlpatterns = i18n_patterns(
+    path("admin/", admin.site.urls),
+    path("", include("articles.urls")),
+)
+
+
+urlpatterns += [
     path("ckeditor/", include("ckeditor_uploader.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += i18n_patterns(
-    path("", ArticleListView.as_view(), name="list"),
-    path("admin/", admin.site.urls),
-    path("article/", include("articles.urls"), name="article"),
-)
